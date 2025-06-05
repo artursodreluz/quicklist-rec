7beb01877eabce9efd0198a2d9e05b1ff1b636c6
EA - Artur Sodré Gomes Da Luz <sodre_gomes@educadventista.org>
2025-06-05 12:17:01 -0300

    validação do preenchimento da input para criação de um novo item

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const items = []
 
 function addItem() {
-    const itemName = 
-    document.querySelector("#item").value
+    const itemName =  document.querySelector("#item").value
 
+    if (itemName === "") {
+        alert("Digite um item válido!")
+        return
+    }
+    
     const item = {
     name: itemName,
     checked: false
